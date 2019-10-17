e3379b4f1a50537582ce29867866650540c1a604
David Plews <cleargif@users.noreply.github.com>
2019-10-17 21:04:32 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,7 @@ const Navbar = class extends React.Component {
           </div>
           <div
             id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+            className={`navbar-menu ${this.state.navBarActiveClass} is-hidden`}
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
