c3ee9c8539c0f9f5443bccd12704e09aff297ae9
David Plews <cleargif@users.noreply.github.com>
2019-10-17 21:30:31 +0100

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -39,9 +39,6 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
